71cafb574ad3ebfe7969f89506d5bc53d8bf7dfe
tonyjleal <tonyjleal@gmail.com>
2021-12-05 17:43:40 +0100

    Menu literal console log

--- 04-tareas-hacer/app.js
+++ 04-tareas-hacer/app.js
@@ -1,9 +1,18 @@
 require('colors');
+
+const { mostrarMenu, pausa } = require('./helpers/mensajes');
+
 console.clear();
 
 const main = async() => {
-    console.log('Hola Mundo');
 
+    let opt = '';
+
+    do {
+       opt = await mostrarMenu();
+       if( opt !== '0' ) await pausa();
+    } while( opt !== '0' );
 }
+
 
 main();
