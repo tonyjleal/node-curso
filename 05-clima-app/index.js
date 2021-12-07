840b069821977819d17cf517bf825f512c9fa957
tonyjleal <tonyjleal@gmail.com>
2021-12-07 01:04:12 +0100

    Menu application

--- 05-clima-app/index.js
+++ 05-clima-app/index.js
@@ -1,10 +1,18 @@
-const { readInput } = require('./helpers/inquirer')
+const { readInput, pause, inquireMenu } = require('./helpers/inquirer')
 require('colors');
 
 const main = async() => {
 
-    const texto = await readInput('Inserte texto: ');
-    console.log(texto);
+    let opt;
+
+    
+    do {
+        opt = await inquireMenu();
+        console.log({opt});
+
+        if(opt!== 0) await pause();
+    } while (opt !== 0)
+
 }
 
 main();
