ed8dcc6005432fb98b509c4873a73a9ee226fe81
tonyjleal <tonyjleal@gmail.com>
2021-12-09 17:40:51 +0100

    Add npm handlebars (hbs)

--- 06-webServer/app.js
+++ 06-webServer/app.js
@@ -2,7 +2,14 @@ const express = require('express');
 const app = express();
 const port = 8081; 
 
+// Renderizar vistas sencillas
+app.set('view engine', 'hbs');
+
 app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
 
 app.get('/generic', (req, res) => {
   res.sendFile(__dirname + '/public/generic.html' );
