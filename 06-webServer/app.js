e1c8255d3e76ff988cf9fa61e8c5c141edbbf3af
tonyjleal <tonyjleal@gmail.com>
2021-12-10 13:21:53 +0100

    Add Angular and deploy to Heroku

--- 06-webServer/app.js
+++ 06-webServer/app.js
@@ -8,31 +8,33 @@ const port = process.env.PORT;
 // Handlebars
 app.set('view engine', 'hbs');
 hbs.registerPartials(__dirname.concat('/views/partials'));
+
+// Servir contenido estático
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.render('home', {
-    nombre: 'Antonio Leal',
-    titulo: 'Curso de Node'
-  });
-});
+// app.get('/', (req, res) => {
+//   res.render('home', {
+//     nombre: 'Antonio Leal',
+//     titulo: 'Curso de Node'
+//   });
+// });
 
-app.get('/generic', (req, res) => {
-  res.render('generic', {
-    nombre: 'Antonio Leal',
-    titulo: 'Curso de Node'
-  });
-});
+// app.get('/generic', (req, res) => {
+//   res.render('generic', {
+//     nombre: 'Antonio Leal',
+//     titulo: 'Curso de Node'
+//   });
+// });
 
-app.get('/elements', (req, res) => {
-  res.render('elements', {
-    nombre: 'Antonio Leal',
-    titulo: 'Curso de Node'
-  });
-});
+// app.get('/elements', (req, res) => {
+//   res.render('elements', {
+//     nombre: 'Antonio Leal',
+//     titulo: 'Curso de Node'
+//   });
+// });
 
 app.get('*', (req, res) => {
-  res.sendFile(__dirname + '/public/404.html' );
+  res.sendFile(__dirname + '/public/index.html' );
 });
 
 app.listen(port, () => {
