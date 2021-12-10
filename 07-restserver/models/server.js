4063ea155c459eaebbe9540c0ba56d025de0d9b0
tonyjleal <tonyjleal@gmail.com>
2021-12-10 13:57:26 +0100

    ENDPOINTS HTTP

--- 07-restserver/models/server.js
+++ 07-restserver/models/server.js
@@ -19,8 +19,35 @@ class Server {
     }
 
     routes() {
+
         this.app.get('/api', (req, res) => {
-            res.send('Hello World');
+            res.json({
+                message: 'get API'
+            });
+        });
+
+        this.app.put('/api', (req, res) => {
+            res.json({
+                message: 'put API'
+            });
+        });
+
+        this.app.post('/api', (req, res) => {
+            res.json({
+                message: 'post API'
+            });
+        });
+
+        this.app.delete('/api', (req, res) => {
+            res.json({
+                message: 'delete API'
+            });
+        });
+
+        this.app.patch('/api', (req, res) => {
+            res.json({
+                message: 'patch API'
+            });
         });
     }
 
