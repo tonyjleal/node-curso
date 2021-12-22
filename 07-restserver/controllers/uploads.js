1d1f57dfde2862af2cf5bdca8857b62a2d10dd32
tonyjleal <tonyjleal@gmail.com>
2021-12-22 23:55:17 +0100

    Refactoring - create Helper to uploadFile

--- 07-restserver/controllers/uploads.js
+++ 07-restserver/controllers/uploads.js
@@ -1,9 +1,8 @@
-const path = require('path');
-const { v4: uuidv4 } = require('uuid');
 const { response } = require("express");
+const { uploadFile } = require("../helpers");
 
 
-const loadFile = ( req, res = response) => {
+const loadFile = async( req, res = response) => {
   
     if (!req.files || Object.keys(req.files).length === 0 || !req.files.file) {
       res.status(400).json({
@@ -12,30 +11,12 @@ const loadFile = ( req, res = response) => {
       return;
     }
   
- 
-    const { file } = req.files;
-    const shortName = file.name.split('.');
-    const extension = shortName[ shortName.length -1 ];
-    
-    const extensionAllowed = ['png', 'jpg', 'jpeg', 'gif'];
-    
-    if ( !extensionAllowed.includes(extension) ) {
-        return res.status(400).json({
-            msg: `La extensión ${ extension } no es permitida`,
-        });
-    }
-    
-    const temporalName = uuidv4() + '.'+ extension;
-    const uploadPath = path.join(__dirname, '../uploads/', temporalName);
-  
-    file.mv(uploadPath, (err) => {
-      if (err) {
-        return res.status(500).json({ err });
-      }
-  
-      res.json({
-          msg: 'File uploaded to ' + uploadPath });
-    });
+    const fileName = await uploadFile(req.files);
+
+    res.json({
+        fileName,
+    })
+
 }
 
 
---
+++ 07-restserver/helpers/upload-file.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+
+const uploadFile = ( files, folder = '', extensionAllowed = ['png', 'jpg', 'jpeg', 'gif'] ) => {
+
+    return new Promise( (resolve, reject ) => {
+
+        const { file } = files;
+        const shortName = file.name.split('.');
+        const extension = shortName[ shortName.length -1 ];
+        
+        if ( !extensionAllowed.includes(extension) ) {
+            return reject(`La extensión ${ extension } no es permitida`)
+        }
+        
+        const temporalName = uuidv4() + '.'+ extension;
+        const uploadPath = path.join(__dirname, '../uploads/', folder, temporalName);
+    
+        file.mv(uploadPath, (err) => {
+            if (err) {
+                return reject( err );
+            }
+        
+            resolve(temporalName);
+        });
+    });
+}
+
+module.exports = {
+    uploadFile,
+}
