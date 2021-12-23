069476ccc37f52865a6a122c35b55f2bcbc47f3e
tonyjleal <tonyjleal@gmail.com>
2021-12-24 00:01:56 +0100

    Path Get for images to show it

--- 07-restserver/controllers/uploads.js
+++ 07-restserver/controllers/uploads.js
@@ -71,7 +71,52 @@ const updateImage = async(req, res = response) => {
 }
 
 
+const showImage = async( req, res= response ) => {
+
+    const { id, collection } = req.params;
+
+    let model;
+
+    switch ( collection ) {
+        case 'users':
+            model = await User.findById(id);
+            if( !model ) {
+                return res.status(400).json({
+                    msg: `No existe usuario con el id ${ id }`
+                }); 
+            }
+        break;
+        case 'products':
+            model = await Product.findById(id);
+            if( !model ) {
+                return res.status(400).json({
+                    msg: `No existe el producto con el id ${ id }`
+                })
+            }
+        break;
+        default:
+            return res.status(500).json({
+                msg: 'No está realizada esta validación'
+            });
+    }
+
+    // Limpiar imagenes previas
+
+    if( model.image ) {
+        // Borrar imagen del servidor
+        const pathImage = path.join(__dirname, '../uploads', collection, model.image)
+        if ( fs.existsSync(pathImage) ) {
+            return res.sendFile( pathImage );
+        } 
+    }
+
+    res.json({
+        msg: 'Falta place holder'
+    });
+}
+
 module.exports = {
     loadFile,
-    updateImage
+    updateImage,
+    showImage
 }
--- 07-restserver/routes/uploads.js
+++ 07-restserver/routes/uploads.js
@@ -1,11 +1,16 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
-const { loadFile, updateImage } = require('../controllers/uploads');
+const { loadFile, updateImage, showImage } = require('../controllers/uploads');
 const { collectionsAllowed } = require('../helpers');
 const { validateFields, validateFileUpload } = require('../middlewares');
 
 const router = Router();
 
+router.get('/:collection/:id', [
+    check('id', 'El id debe ser de mongo').isMongoId(),
+    check('collection').custom( c => collectionsAllowed( c, ['users', 'products']) ),
+    validateFields 
+], showImage);
 
 router.post( '/', [
     validateFileUpload,
