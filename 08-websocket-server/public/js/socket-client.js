3b24a75532fc322c7e1f860a587722b777958247
tonyjleal <tonyjleal@gmail.com>
2021-12-26 20:54:56 +0100

    Messages's connection & disconnection

--- 08-websocket-server/public/js/socket-client.js
+++ 08-websocket-server/public/js/socket-client.js
@@ -1,4 +1,26 @@
 //Configuración y comunicación con el servidor
 
+// Referencias HTML
+const lblOnline  = document.querySelector('#lblOnline'); 
+const lblOffline = document.querySelector('#lblOffline');
+
 const socket = io();
+
+socket.on('connect', () => {
+
+    console.log('Connected');
+
+    lblOffline.style.display = 'none';
+    lblOnline.style.display = '';
+
+});
+
+socket.on('disconnect', () => {
+
+    console.log('Disconnected');
+
+    lblOffline.style.display = '';
+    lblOnline.style.display = 'none';
+
+});
 
