d4fe19c1885d709709b75cd9283bd45f920bd76f
tonyjleal <tonyjleal@gmail.com>
2021-12-29 22:45:33 +0100

    Configure desktop screen

--- 09-socket-colas/public/js/desktop.js
+++ 09-socket-colas/public/js/desktop.js
@@ -1,3 +1,49 @@
+// Referencias HTML
+const lblDesktop = document.querySelector('h1');
+const btnAttend = document.querySelector('button');
 
 
-console.log('Escritorio HTML');
+
+const searchParams = new URLSearchParams( window.location.search );
+
+if( !searchParams.has('desktop') ) {
+    window.location = 'index.html';
+    throw new Error('El escritorio es oblitatorio');
+}
+
+const desktop = searchParams.get('desktop');
+lblDesktop.innerText = desktop;
+
+// Referencias del HTML
+const lblNewTicket  = document.querySelector('#lblNewTicket');
+const btnCreate = document.querySelector('button');
+
+const socket = io();
+
+
+
+socket.on('connect', () => {
+
+    btnAttend.disabled = false;
+
+});
+
+socket.on('disconnect', () => {
+
+    btnAttend.disabled = true;
+
+});
+
+
+socket.on( 'last-ticket',  ( last ) => {
+    // lblNewTicket.innerText = last;
+});
+
+btnAttend.addEventListener( 'click', () => {
+    
+    // socket.emit( 'next-ticket', null, ( ticket ) => {
+    //     lblNewTicket.innerText = ticket;
+    // });
+
+});
+
