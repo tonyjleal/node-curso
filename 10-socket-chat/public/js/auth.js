d0e4c6b224cd3899b5f900ad068aa1b6f23ee52a
tonyjleal <tonyjleal@gmail.com>
2022-01-01 17:20:56 +0100

    UI Login and set token in localStorage

---
+++ 10-socket-chat/public/js/auth.js
@@ -0,0 +1,65 @@
+const myForm = document.querySelector('form');
+const url = (window.location.hostname.includes('localhost')) 
+            ? 'http://localhost:8081/api/auth'
+            : 'https://curso-node-restserver-tj.herokuapp.com/api/auth';
+
+
+
+myForm.addEventListener('submit', ev => {
+    ev.preventDefault();
+    const formData = {};
+
+    for( let el of myForm.elements ) {
+        if( el.name.length > 0 ) {
+            formData[el.name] = el.value;
+        }
+    }
+    
+    fetch( `${ url }/login`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+    })
+    .then( resp => resp.json() )
+    .then( ({ msg, token }) => {
+        if( msg ) {
+           return console.error(msg);
+        }
+        
+        localStorage.setItem('token', token);
+
+    })
+    .catch( err => {
+        console.log( err );
+    });
+});
+
+function handleCredentialResponse(response) {
+            
+    //Google Token : ID_TOKEN
+    // console.log('id_token', response.credential);
+    const body = { id_token: response.credential };
+    
+    fetch(`${ url }/google`,  {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    }).then( resp => resp.json())
+        .then( ({ token }) => {
+            localStorage.setItem( 'token', token );
+        })
+        .catch( console.warn );
+}
+
+const button = document.getElementById('google_signout');
+button.onclick = () => {
+    google.accounts.id.disableAutoSelect()
+    google.accounts.id.revoke(localStorage.getItem('email'), done => {
+        localStorage.clear();
+        location.reload();
+    })
+}
