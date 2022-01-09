78f939876f74052403e5113f6ced528a43f64d2f
tonyjleal <tonyjleal@gmail.com>
2022-01-09 23:01:14 +0100

    Send message broadcast

--- 12-project-chat/public/js/socket-chat.js
+++ 12-project-chat/public/js/socket-chat.js
@@ -29,12 +29,12 @@ socket.on('disconnect', function() {
 
 
 // Enviar información
-socket.emit('enviarMensaje', {
-    usuario: 'Fernando',
-    mensaje: 'Hola Mundo'
-}, function(resp) {
-    console.log('respuesta server: ', resp);
-});
+// socket.emit('sendMessage', {
+//     usuario: 'Fernando',
+//     mensaje: 'Hola Mundo'
+// }, function(resp) {
+//     console.log('respuesta server: ', resp);
+// });
 
 // Escuchar información
 socket.on('createMessage', function(mensaje) {
--- 12-project-chat/server/sockets/socket.js
+++ 12-project-chat/server/sockets/socket.js
@@ -1,6 +1,6 @@
 const { Users } = require('../classes/users');
 const { io } = require('../server');
-
+const { createMessage } = require('../utils/utils');
 const users = new Users();
 
 io.on('connection', (client) => {
@@ -21,10 +21,17 @@ io.on('connection', (client) => {
     client.on('disconnect', () => {
         let userDisconnect =  users.deleteUser(client.id);
         
-        client.broadcast.emit('createMessage', { user:'Admin', message: `${userDisconnect.name} abandonó el chat`});
+        client.broadcast.emit('createMessage', createMessage('Admin',`${userDisconnect.name} abandonó el chat`));
         client.broadcast.emit('listUsers', users.getUsers());
     });
 
+    client.on('sendMessage', (data) => {
 
+        let user = users.getUser(client.id);
+        let message = createMessage(user.name, data.message);
+
+        client.broadcast.emit('createMessage', message);
+
+    });
 
 });
