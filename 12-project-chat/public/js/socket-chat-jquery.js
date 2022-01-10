a760078245574135ccb25251f93bc9d0fa237df7
tonyjleal <tonyjleal@gmail.com>
2022-01-10 11:06:40 +0100

    UI Web

---
+++ 12-project-chat/public/js/socket-chat-jquery.js
@@ -0,0 +1,25 @@
+let params = new URLSearchParams(window.location.search);
+// 
+
+//References
+const divUsers = $('#divUsers');
+
+const renderUsers = (users) => {
+    console.log(users);
+
+    let html = '';
+
+    html = `<li>
+                <a href="javascript:void(0)" class="active"> Chat de <span> ${ params.get('room') }</span></a>;
+            </li>`;
+
+
+    for(let i = 0; i < users.length; ++i) {
+       html += `<li>
+                    <a  data-id="${ users[i].id }"  href="javascript:void(0)"><img src="assets/images/users/1.jpg" alt="user-img" class="img-circle"> <span>${ users[i].name }<small class="text-success">online</small></span></a>
+                </li>`;
+    }        
+
+    divUsers.html(html);
+
+}
